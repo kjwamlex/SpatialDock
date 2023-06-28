e2febe340c37cf2ecaefd5636a5595d2fb106c9b
kjwamlex <kjwamlex@gmail.com>
2023-06-28 19:44:19 -0400

    Initial Commit

---
+++ VisionDock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionDock
+//
+//  Created by Joonwoo KIM on 2023-06-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionDock/VisionDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionDockApp.swift
+//  VisionDock
+//
+//  Created by Joonwoo KIM on 2023-06-28.
+//
+
+import SwiftUI
+
+@main
+struct VisionDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
