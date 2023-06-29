db06a0b8c92fcc0f3a8bb2a5dcd78c0cdaaaaf05
kjwamlex <kjwamlex@gmail.com>
2023-06-29 10:50:24 -0400

    code commits

--- VisionDock/ContentView.swift
+++ VisionDock/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let systemApps:[String] = ["Safari", "Settings", "Files"]
+    let appsCorrespondingURL:[String : String] = ["Safari" : "x-web-search://", "Settings": UIApplication.openSettingsURLString, "Files" : "shareddocuments://"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                ForEach(systemApps, id: \.self) { app in
+                    VStack {
+                        Button {
+                            if let url = URL(string: appsCorrespondingURL[app] ?? "") {
+                                if UIApplication.shared.canOpenURL(url) {
+                                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                                } else {
+                                    print("cant")
+                                }
+                            }
+                        } label: {
+                            Image(app)
+                        }
+                        .buttonStyle(.borderless)
+                        .buttonBorderShape(.circle)
+                        Text(app)
+                    }
+                }
+            }
         }
-        .padding()
+        .padding(10)
+        Spacer()
+        //VStack {
+            // Photos app - photos-navigation://
+            // Maps app - map://
+            // Safari - x-web-search://
+            // Files - shareddocuments://
+            // Home - homeutil://
+            // News - applenews://
+            // Shortcuts - pocketapp36486://
     }
 }
 
