19ffe3a8cc92ffe72662013bd74a171e24fc1b8c
exDeveloper <kjwamlex@gmail.com>
2023-07-26 01:18:57 -0400

    More specifics and directions and tweaks in Settings UI

---
+++ VisionDock/Settings/SettingsSubscription.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsSubscription.swift
+//  SpatialDock
+//
+//  Created by Joonwoo KIM on 2023-07-26.
+//
+
+import SwiftUI
+
+struct SettingsSubscription: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsSubscription()
+}
